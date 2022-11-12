7c3ecec0ed397102ca6f6c512091a5ff60a46806
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-11-12 12:36:12 +0100

    fix: remove connected user in contacts list

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         <div className="flex flex-col justify-start mb-1">
             <h1 className="text-3xl text-teal-800 font-black m-4">Contacts</h1>
             <div className="flex ml-5 overflow-scroll">
-                {data.map(({ _id, username }) => <div key={_id} className="flex flex-col items-center mx-1">
+                {data.map(({ _id, username }) => _id !== localStorage.getItem('id') && <div key={_id} className="flex flex-col items-center mx-1">
                     <HiUser className="h-12 w-12 text-gray-500 border border-teal-600 p-1 rounded-full" />
                     <p className="mt-1 font-semibold w-[40px] overflow-hidden">{username}</p>
                 </div>)}
