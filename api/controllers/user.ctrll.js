80a417a869335e53c916723bcbc1fa7a3147831b
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-11-12 13:11:32 +0100

    style: UI-adding search contact bar

--- api/controllers/user.ctrll.js
+++ api/controllers/user.ctrll.js
@@ -39,6 +39,7 @@ export const login = async (req, res, next) => {
             }
             res.status(200).json({
                 userId: user._id,
+                username: user.username,
                 token: jwt.sign(
                     { userId: user._id }, process.env.TOKEN_KEY, { expiresIn: '6h' }
                 )
--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import { HiUser } from "react-icons/hi";
+import { IoIosSearch } from "react-icons/io";
 import { useStateContext } from "../context/ContextProvider";
 import { fetchData } from "../hook/useFecth";
 
@@ -7,7 +8,25 @@ const Contact = () => {
     const [data] = fetchData(users, setUsers, '/users');
     return (
         <div className="flex flex-col justify-start mb-1">
-            <h1 className="text-3xl text-teal-800 font-black m-4">Contacts</h1>
+            <div className="flex justify-between items-center m-4 mb-0">
+                <h1 className="text-3xl text-teal-800 font-black">Contacts</h1>
+                <HiUser className="h-7 w-7 text-gray-500 rounded-full" />
+            </div>
+            <div className='bg-gray-200 flex justify-between items-center relative mx-4 mb-4 rounded-full'>
+                <span className='absolute left-3 mr-3 hover:cursor-pointer text-lg text-gray-500'>
+                    <IoIosSearch />
+                </span>
+                <input
+                    className={`focus:outline-none rounded-full border py-1 pr-4 pl-10 block appearance-none w-full`}
+                // type={type}
+                // value={value}
+                // placeholder={placeholder}
+                // onChange={onChange}
+                // name={name}
+                // disabled={disabled}
+                >
+                </input>
+            </div>
             <div className="flex ml-5 overflow-scroll">
                 {data.map(({ _id, username }) => _id !== localStorage.getItem('id') && <div key={_id} className="flex flex-col items-center mx-1">
                     <HiUser className="h-12 w-12 text-gray-500 border border-teal-600 p-1 rounded-full" />
