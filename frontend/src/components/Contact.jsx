62ae2d1334da3333d3eac1d191ace829935a5f66
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-11-16 13:44:26 +0100

    refact: UI-remove conditionnal display in contact file

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -48,31 +48,28 @@ const Contact = () => {
                 >
                 </input>
             </div>
-            {
-                boolingState.showContactList &&
-                <div className="flex flex-col ml-5 overflow-clip absolute right-0 top-14 bg-teal-600 text-gray-100 p-4 shadow-xl w-full rounded-xl">
-                    <div
-                        className='flex self-end justify-end mb-4 px-3 rounded-full bg-teal-200 text-teal-800 hover:text-teal-200 hover:bg-teal-800 shadow-2xl cursor-pointer'
-                        onClick={handleShowContactList}
-                    >
-                        <p className=''>Fermer</p>
-                    </div>
-                    {
-                        usersData.length > 0 ? usersData.map((receiver) => receiver._id !== userData._id &&
-                            <div
-                                key={receiver._id}
-                                className="flex justify-between items-center mx-1 mb-2 pb-1 hover:cursor-pointer border-b border-gray-300"
-                                onClick={() => {
-                                    fetchMessages(receiver._id, JSON.stringify(receiver), setMessagesList, setBoolingState, boolingState);
-                                    handleShowContactList();
-                                }}
-                            >
-                                <p className="font-semibold">{receiver.username}</p>
-                                <HiUser className="h-10 w-10 text-yellow-500 border border-teal-200 p-1 rounded-full" />
-                            </div>) : <div className='text-red-500'>No user found</div>
-                    }
+            <div className="flex flex-col ml-5 overflow-clip absolute right-0 top-14 bg-teal-600 text-gray-100 p-4 shadow-xl w-full rounded-xl">
+                <div
+                    className='flex self-end justify-end mb-4 px-3 rounded-full bg-teal-200 text-teal-800 hover:text-teal-200 hover:bg-teal-800 shadow-2xl cursor-pointer'
+                    onClick={handleShowContactList}
+                >
+                    <p className=''>Fermer</p>
                 </div>
-            }
+                {
+                    usersData.length > 0 ? usersData.map((receiver) => receiver._id !== userData._id &&
+                        <div
+                            key={receiver._id}
+                            className="flex justify-between items-center mx-1 mb-2 pb-1 hover:cursor-pointer border-b border-gray-300"
+                            onClick={() => {
+                                fetchMessages(receiver._id, JSON.stringify(receiver), setMessagesList, setBoolingState, boolingState);
+                                handleShowContactList();
+                            }}
+                        >
+                            <p className="font-semibold">{receiver.username}</p>
+                            <HiUser className="h-10 w-10 text-yellow-500 border border-teal-200 p-1 rounded-full" />
+                        </div>) : <div className='text-red-500'>No user found</div>
+                }
+            </div>
         </div>
     );
 }
