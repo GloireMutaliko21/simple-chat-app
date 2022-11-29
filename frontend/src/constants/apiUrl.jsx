bee99efc6080e4b4dc54fb50ba9ffb1fba4e71c3
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-11-29 11:21:47 +0100

    refact: UI-using online API

--- frontend/src/constants/apiUrl.jsx
+++ frontend/src/constants/apiUrl.jsx
@@ -1,1 +1,2 @@
-export const API_URL = `http://localhost:${import.meta.env.VITE_API_PORT}/talks/api/v1`
+export const API_URL = import.meta.env.VITE_API_URL
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
