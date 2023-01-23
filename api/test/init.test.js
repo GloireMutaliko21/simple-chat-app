12769008d99aae66db5c340076acaea3f9eba0e7
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-23 09:02:04 +0100

    test: get all users

--- api/test/init.test.js
+++ api/test/init.test.js
@@ -8,13 +8,28 @@ chai.use(chaiHttp);
 const should = chai.should();
 const expect = chai.expect;
 
+let token;
+
 describe('LUNCH TEST', () => {
-    //Test API
+    //Test Login
     it('Should connect', (done) => {
         chai
             .request(app)
             .post('/talks/api/v1/users/login')
             .send({ email: 'val@golla.com', password: 'val' })
+            .end((err, res) => {
+                expect(res.status).to.equal(200);
+                token = res.body.token;
+                done();
+            });
+    });
+
+    //Test Get all users
+    it('Get all users', (done) => {
+        chai
+            .request(app)
+            .get('/talks/api/v1/users')
+            .auth(token, { type: 'bearer' })
             .end((err, res) => {
                 expect(res.status).to.equal(200);
                 done();
